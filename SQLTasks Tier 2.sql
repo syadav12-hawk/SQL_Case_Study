f21f8e1f9c1aedc09f620173d551e8c1648c92a9
syadav12 <syadav12@hawk.iit.edu>
2021-04-06 17:12:11 -0400

    Answered PHPAdmin SQL queries

--- SQLTasks Tier 2.sql
+++ SQLTasks Tier 2.sql
@@ -35,8 +35,17 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
+SELECT name
+FROM Facilities
+WHERE membercost = 0.0
+LIMIT 0 , 30
+
 
 /* Q2: How many facilities do not charge a fee to members? */
+
+SELECT COUNT( * )
+FROM Facilities
+WHERE membercost = 0.0
 
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
@@ -45,8 +54,21 @@ Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost <> 0.0
+AND membercost < monthlymaintenance * 0.2
+LIMIT 0 , 30
+
+
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
+
+SELECT *
+FROM Facilities
+WHERE facid
+IN ( 1, 5 )
+LIMIT 0 , 30
 
 
 /* Q5: Produce a list of facilities, with each labelled as
@@ -54,15 +76,39 @@ Try writing the query without using the OR operator. */
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name, monthlymaintenance,
+CASE WHEN monthlymaintenance >100
+THEN 'expensive'
+ELSE 'cheap'
+END AS
+type
+FROM Facilities
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
+
+SELECT *
+FROM Members
+WHERE joindate
+IN (
+SELECT MAX( joindate )
+FROM Members
+)
 
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
+
+SELECT DISTINCT f.name, CONCAT( m.firstname, " ", m.surname ) AS mem_name
+FROM Bookings AS b
+LEFT JOIN Facilities AS f ON b.facid = f.facid
+LEFT JOIN Members AS m ON b.memid = m.memid
+WHERE b.facid
+IN ( 0, 1 )
+AND m.memid <>0
+ORDER BY mem_name
 
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
